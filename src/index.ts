3736eab9e03a7802643baa0d1718c0c145be513b
hapo31 <happo31@gmail.com>
2020-11-15 17:39:31 +0900

    feat: add ipcMain handlers

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { app, BrowserWindow, dialog, ipcMain } from "electron";
+import { app, BrowserWindow, dialog, ipcMain, screen } from "electron";
 
 class OCRApp {
 
   private window: BrowserWindow | null = null;
-  
+
   constructor() {
     app.on("ready", () => {
       this.window = new BrowserWindow({
+        width: 1280,
+        height: 960,
         webPreferences: {
           nodeIntegration: true
         }
@@ -14,7 +16,6 @@ class OCRApp {
 
       this.window.loadURL(`file://${__dirname}/index.html`);
       this.window.webContents.openDevTools();
-      
     });
 
     app.on("window-all-closed", () => {
@@ -36,6 +37,24 @@ class OCRApp {
       }
 
       return result.filePaths[0];
+    });
+
+    ipcMain.handle("get-content-size", () => {
+      return this.window?.getContentSize();
+    });
+
+    ipcMain.handle("get-display-dpi", () => {
+      if (this.window == null) {
+        return;
+      }
+      const [x, y] = this.window.getPosition();
+      const display = screen.getDisplayNearestPoint({x, y});
+
+      return display.scaleFactor;
+    });
+
+    ipcMain.handle("set-window-size", (_, width: number, height: number) => {
+      this.window?.setContentSize(width, height);
     });
   }
 }
