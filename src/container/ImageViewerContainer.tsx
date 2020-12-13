86629d2a0922a0d29417edfffc1ca7713016be56
happou31 <happo31@gmail.com>
2020-12-14 00:20:27 +0900

    fix: マウスオーバーしたときに出るやつなくてもよくない？

--- src/container/ImageViewerContainer.tsx
+++ src/container/ImageViewerContainer.tsx
@@ -1,7 +1,5 @@
 import React, { useCallback, useContext } from "react";
 import ImageCutter from "../components/ImageCutter";
-import Rectangle from "../domain/Recrangle";
-import { AddRect, useImageReducer } from "../reducer/useImageReducer";
 import { AppContext } from "./App";
 
 type Props = {
@@ -11,18 +9,9 @@ type Props = {
 export default (props: Props) => {
   const appState = useContext(AppContext);
 
-  const [state, dispatchImageState] = useImageReducer({
-    rectangles: [],
-    fileType: appState.fileType,
-  });
-
-  const onAddRect = useCallback(
-    async (rect: Rectangle, resultImage: HTMLCanvasElement) => {
-      dispatchImageState(AddRect(rect));
-      props.onAddTask(resultImage);
-    },
-    []
-  );
+  const onAddRect = useCallback(async (resultImage: HTMLCanvasElement) => {
+    props.onAddTask(resultImage);
+  }, []);
 
   const onLoadImage = useCallback(
     async (event: React.ChangeEvent<HTMLImageElement>) => {
@@ -34,9 +23,7 @@ export default (props: Props) => {
   return (
     <ImageCutter
       fileType={appState.fileType}
-      rectangles={state.rectangles}
       src={appState.imageSrc || ""}
-      showRectangleIndex={appState.showRectangleIndex}
       onAddRect={onAddRect}
       onLoad={onLoadImage}
     />
